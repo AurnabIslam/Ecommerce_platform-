29dac8186c11bee5077b21d0af530d1e3000897f
AurnabIslam <55218093+AurnabIslam@users.noreply.github.com>
2019-12-08 02:02:23 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+**WDDM: Final Composite Project**
+# Web "Store" Application Interface
+
+---
+
+## Overview
+
+Develop a single-page interactive web application to manage an inventory of products for sale. The theme of the website will be of your choosing; You may sell any type of product.
+
+Using the provided HTML document (`index.html`) as a *starting template only*, you will develop unique content and design for 20 products and display them in an HTML document dynamically. 
+
+From that point, under the direction of your course instructors, you will design and develop: the application's design system, user experience, development tooling, interface, and functionality. Each student will have unique outcomes and challenges based on the chosen theme and the ability to implement a creative solution.
+
+---
+
+## To Get Started Now
+
+Every student should complete these steps as soon as possible:
+
+1. **Fork this repository** by clicking the "Fork" button in the top-right corner of this repository to make a copy of the project to your Github account (must be logged in)
+   - After forking, ensure your Github account name is shown ahead of the repository name (and the page url), for example: `your-github-name / webstore` (where `your-github-name` is your Github username, *not `wddm-f19`!*)
+2. **Clone the forked repository** (From VSCode: `Cmd + Shift + p`, search for "Git: Clone repo". Paste *your* repo url. Save to your desktop)
+3. **Make the website site live** by visiting "Settings" (from the repo homepage's sub menu (at the top), or add `/settings` to your repo's home url). Scroll to the "Github Pages" section and change the "Source" dropdown to show "master branch" *(default is likely set to "None")*.
+4. **Add the live site's url to the repo home page** by revisiting "Settings" > "Github Pages" and copying the page URL (it should be similar to `https://[your-github-name].github.io/webstore/`). On repo's home page, edit the repo description and paste the URL in the "website" input field, then save.
+
+Your repository will by tracked by the original as a fork, which will be used as your submission on the due dates automatically. All work must be committed and pushed to that repository to submit - no exceptions. 
+
+> *Remember: If anything goes wrong with your repo at any point, you can: download the code, delete the repo from your account, re-fork, clone, compare your local code to the downloaded version and paste in the source code that's most recent, commit, then push.*
+
+You may keep your repository "private" until prior to submission, but may be asked to share code or documents with your instructors for assessment prior to submission. Ensure you are committing frequently and backing up your work to a remote after each work session.
+
+---
+
+## Submission & Grading
+
+Each instructor will provided details on their respective requirements.
---
+++ js/index.js
@@ -0,0 +1,300 @@
+//Product Data
+var product1 = {
+    name:'Shirt #1',
+    imgURL:'img/tshirt_1.jpg',
+    color1:'Grey',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 39.99
+};
+var product2 = {
+    name:'Shirt #2',
+    imgURL:'img/tshirt_2.jpg',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'XS',
+    size2:'S',
+    size3:'M',
+    rating:'4',
+    price: 19.99
+};
+var product3 = {
+    name:'Shirt #3',
+    imgURL:'img/tshirt_3.jpg',
+    color1:'White',
+    color2:'Red',
+    color3:'Blue',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 29.99
+};
+var product4 = {
+    name:'Shirt #4',
+    imgURL:'img/tshirt_4.jpg',
+    color1:'White',
+    color2:'Blue',
+    color3:'Black',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 25.99
+};
+var product5 = {
+    name:'Shirt #5',
+    imgURL:'img/tshirt_5.jpg',
+    color1:'Grey',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 35.99
+};
+var product6 = {
+    name:'Shirt #6',
+    imgURL:'#',
+    color1:'Black',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 39.99
+};
+var product7 = {
+    name:'Shirt #7',
+    imgURL:'#',
+    color1:'Grey',
+    color2:'Black',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 29.99
+};
+var product8 = {
+    name:'Shirt #8',
+    imgURL:'#',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'XS',
+    size2:'S',
+    size3:'M',
+    rating:'4',
+    price: 25.99
+};
+var product9 = {
+    name:'Shirt #9',
+    imgURL:'#',
+    color1:'Red',
+    color2:'Blue',
+    color3:'White',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 29.99
+};
+var product10 = {
+    name:'Shirt #10',
+    imgURL:'#',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 29.99
+};
+var product11 = {
+    name:'Shirt #11',
+    imgURL:'#',
+    color1:'Grey',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 39.99
+};
+var product12 = {
+    name:'Shirt #12',
+    imgURL:'#',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'XS',
+    size2:'S',
+    size3:'M',
+    rating:'4',
+    price: 19.99
+};
+var product13 = {
+    name:'Shirt #13',
+    imgURL:'#',
+    color1:'White',
+    color2:'Red',
+    color3:'Blue',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 29.99
+};
+var product14 = {
+    name:'Shirt #14',
+    imgURL:'#',
+    color1:'White',
+    color2:'Blue',
+    color3:'Black',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 25.99
+};
+var product15 = {
+    name:'Shirt #15',
+    imgURL:'#',
+    color1:'Grey',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 35.99
+};
+var product16 = {
+    name:'Shirt #16',
+    imgURL:'#',
+    color1:'Black',
+    color2:'Red',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 39.99
+};
+var product17 = {
+    name:'Shirt #17',
+    imgURL:'#',
+    color1:'Grey',
+    color2:'Black',
+    color3:'Blue',
+    size1:'M',
+    size2:'L',
+    size3:'XL',
+    rating:'4',
+    price: 29.99
+};
+var product18 = {
+    name:'Shirt #18',
+    imgURL:'#',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'XS',
+    size2:'S',
+    size3:'M',
+    rating:'4',
+    price: 25.99
+};
+var product19 = {
+    name:'Shirt #19',
+    imgURL:'#',
+    color1:'Red',
+    color2:'Blue',
+    color3:'White',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 35.99
+};
+var product20 = {
+    name:'Shirt #20',
+    imgURL:'#',
+    color1:'Black',
+    color2:'White',
+    color3:'Grey',
+    size1:'S',
+    size2:'M',
+    size3:'L',
+    rating:'4',
+    price: 35.99
+};
+
+//Product Images
+document.getElementById('product1-img').src=product1.imgURL;
+document.getElementById('product2-img').src=product2.imgURL;
+document.getElementById('product3-img').src=product3.imgURL;
+document.getElementById('product4-img').src=product4.imgURL;
+document.getElementById('product5-img').src=product5.imgURL;
+
+//Product Names
+document.getElementById('product1-name').innerHTML=product1.name;
+document.getElementById('product2-name').innerHTML=product2.name;
+document.getElementById('product3-name').innerHTML=product3.name;
+document.getElementById('product4-name').innerHTML=product4.name;
+document.getElementById('product5-name').innerHTML=product5.name;
+
+//Product Colors
+document.getElementById('product1-color1-text').textContent=product1.color1;
+document.getElementById('product1-color2-text').textContent=product1.color2;
+document.getElementById('product1-color3-text').textContent=product1.color3;
+
+document.getElementById('product2-color1-text').textContent=product2.color1;
+document.getElementById('product2-color2-text').textContent=product2.color2;
+document.getElementById('product2-color3-text').textContent=product2.color3;
+
+
+document.getElementById('product3-color1-text').textContent=product3.color1;
+document.getElementById('product3-color2-text').textContent=product3.color2;
+document.getElementById('product3-color3-text').textContent=product3.color3;
+
+
+document.getElementById('product4-color1-text').textContent=product4.color1;
+document.getElementById('product4-color2-text').textContent=product4.color2;
+document.getElementById('product4-color3-text').textContent=product4.color3;
+
+
+document.getElementById('product5-color1-text').textContent=product5.color1;
+document.getElementById('product5-color2-text').textContent=product5.color2;
+document.getElementById('product5-color3-text').textContent=product5.color3;
+
+//Product Sizes
+document.getElementById('product1-size1-text').textContent=product1.size1;
+document.getElementById('product1-size2-text').textContent=product1.size2;
+document.getElementById('product1-size3-text').textContent=product1.size3;
+
+document.getElementById('product2-size1-text').textContent=product2.size1;
+document.getElementById('product2-size2-text').textContent=product2.size2;
+document.getElementById('product2-size3-text').textContent=product2.size3;
+
+document.getElementById('product3-size1-text').textContent=product3.size1;
+document.getElementById('product3-size2-text').textContent=product3.size2;
+document.getElementById('product3-size3-text').textContent=product3.size3;
+
+document.getElementById('product4-size1-text').textContent=product4.size1;
+document.getElementById('product4-size2-text').textContent=product4.size2;
+document.getElementById('product4-size3-text').textContent=product4.size3;
+
+document.getElementById('product5-size1-text').textContent=product5.size1;
+document.getElementById('product5-size2-text').textContent=product5.size2;
+document.getElementById('product5-size3-text').textContent=product5.size3;
